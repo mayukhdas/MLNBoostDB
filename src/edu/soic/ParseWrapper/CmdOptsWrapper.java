e7d820bc082a1989256f06354c34fffd10d6ec0a
Mayukh Das <mayukhds@gmail.com>
2018-04-25 11:20:22 -0500

    Updates to wrapper: Transforming BK

--- src/edu/soic/ParseWrapper/CmdOptsWrapper.java
+++ src/edu/soic/ParseWrapper/CmdOptsWrapper.java
@@ -1,5 +1,10 @@
 package edu.soic.ParseWrapper;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 
 public class CmdOptsWrapper {
 	public static final String trainOptionStr = "-l";
@@ -27,10 +32,13 @@ public class CmdOptsWrapper {
 	String aucJarDir;
 	
 	public static final String posNegStr = "-negPosRatio";
-	String negPosRatio;
+	String negPosRatio=null;
 	
 	public static final String databaseStr = "-dt";
 	String DB;
+	
+	
+	public static String bkPathForDB = null;
 	
 	public CmdOptsWrapper(String[] args)
 	{
@@ -64,6 +72,72 @@ public class CmdOptsWrapper {
 				this.DB = args[i+1];
 			}
 		}
+		
+		
 	}
+	
+	public void createWillBk()
+	{
+		String postFix=null;
+		StringBuilder predDefBuild = new StringBuilder();
+		StringBuilder modeBuild = new StringBuilder();
+		File dir = new File(this.trainPath);
+		try 
+		{
+			if(dir.isDirectory())
+			{
+				postFix = dir.getName();
+				System.out.println(postFix);
+			}
+			else
+			{
+				System.out.println("Error in train directory path!!");
+				System.exit(1);
+			}
+			
+			if (postFix.endsWith("/")) {
+				postFix = postFix.substring(0, postFix.length() - 1);
+	        }
+			String bkFilePath = this.trainPath+"/"+postFix+"_bk.txt";
+			
+			File bkFile = new File(bkFilePath);
+			BufferedReader bf = new BufferedReader(new FileReader(bkFile));
+			String line="";
+			if((line=bf.readLine())!=null)
+			{
+				if(line.startsWith("import:"))
+				{
+					String temp = line.split(":")[1].trim();
+					temp = temp.substring(0, temp.length()-1);
+					temp = temp.replace('"', ' ').trim();
+					bkFilePath = this.trainPath+"/"+temp;
+				}
+			}
+			bf.close();
+			bkFile = new File(bkFilePath);
+			bf = new BufferedReader(new FileReader(bkFile));
+			BufferedWriter bw = new BufferedWriter(new FileWriter(new File("./DB_bk.txt")));
+			
+			while((line = bf.readLine())!=null)
+			{
+				if(line.startsWith("mode:"))
+				{
+					line = line.trim();
+					String predDef = line.replaceAll("mode:", "predDef:");
+					predDef.replaceAll("+", "").replaceAll("-", "");
+					predDefBuild.append(predDef+"\n");	
+					
+					modeBuild.append(line+"\n");
+				}
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
 	
 }
